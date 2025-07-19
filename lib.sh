03ae3447ac5c666ce9f7338ccc95aa406fa704c9
1x:tier <soraxsociety@gmail.com>
2025-07-19 04:20:05 -0400

    Add function lib.sh

--- lib.sh
+++ lib.sh
@@ -10,3 +10,16 @@ BLUE='\033[34m'		 # ${BLUE}    ->  # Синий цвет
 MAGENTA='\033[35m'	 # ${MAGENTA} ->  # Фиолетовый цвет
 CYAN='\033[36m'   	 # ${CYAN}    ->  # Цвет морской волны
 GRAY='\033[37m'		 # ${GRAY}    ->  # Серый цвет
+
+
+function rpm_dase(){
+  dnf update -y
+  dnf install -y epel-release || dnf update -y
+  dnf update -y
+  dnf install -y wget mc git vim htop atop traceroute tcpdump nmon atop tree at psmisc ncdu pciutils bind-utils cpan 
+}
+
+function deb_base(){
+  apt update -y && apt upgrade -y
+  apt install wget mc git vim htop atop traceroute tcpdump nmon atop tree at psmisc ncdu pciutils bind-utils cpan
+}
